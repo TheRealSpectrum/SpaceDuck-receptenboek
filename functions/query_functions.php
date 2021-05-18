4dfb18bec2d18a82ec9e0f363f1798ddd67f9dd0
e10ne <etiennevanhalteren@hotmail.com>
2021-05-18 15:46:39 +0200

    new files

---
+++ functions/query_functions.php
@@ -0,0 +1,23 @@
+<?php
+    // voorbeelden sql query:
+
+    /*
+        // aanmaken
+        INSERT INTO tabel_naam (kolom_naam, kolom_naam, etc) VALUES ('waarde', 'waarde', etc);
+
+        // alles selecteren
+        SELECT * FROM tabel_naam;
+
+        // een item met specifieke id selecteren
+        SELECT * FROM tabel_naam WHERE id='id_van_item';
+
+        // wijziging doorvoeren 
+        UPDATE tabel_naam SET kolom_naam='nieuwe_waarde', kolom_naam='nieuwe_waarde', (etc) WHERE id='id_van_item' LIMIT 1;
+
+        // item verwijderen
+        DELETE FROM tabel_naam WHERE id='id_van_item' LIMIT 1;
+
+    */
+
+    
+?>
