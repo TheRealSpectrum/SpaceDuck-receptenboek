43bd12ab6dd454dd82ccffb9e7d5502fb4514b12
Patricia <poudenaarden@outlook.com>
2021-05-27 09:08:48 +0200

    changes form + db

--- create.php
+++ create.php
@@ -1,11 +1,12 @@
 <?php
+    session_start();
     require_once("functions/init.php");
     include("parts/header.php");
 ?>
     <section>
         <h1>Mijn Recept</h1>
 
-        <form>
+        <form action="./parts/insert.php" method="post">
             <label for="maker">Maker</label>
             <input type="text" name="naam_maker"><br>
             <label for="naam recept">Naam Recept</label>
@@ -14,29 +15,19 @@
             <input type="text" name="naam_categorie"><br>
             <label for="aantal personen">Aantal Personen</label>
             <input type="number" name="aantal_personen"><br>
-            <label for="aantal ingredienten">Aantal Ingredienten</label>
-            <input type="number" name="aantal_ingredienten"><br>
-            <input type="submit"> <br><br>
-
-            
-            <label for="naam ingredient">Naam Ingredient</label>
-            <input type="text" name="naam_ingredient"><br>
-            <label for="hoeveelheid">Hoeveelheid</label>
-            <input type="number" name="hoeveelheid"><br>
-            <select name = "eenheid">
-            <option value = "gram" selected>Gram</option>
-            <option value = "kilogram">Kilogram</option>
-            <option value = "milliliter">Milliliter</option>
-            <option value = "deciliter">Deciliter</option>
-            <option value = "centiliter">Centiliter</option>
-            <option value = "liter">Liter</option>
-            <option value = "theelepel">Theelepel</option>
-            <option value = "eetlepel">Eetlepel</option>
-            <option value = "naar smaak">Naar Smaak</option>
-            
-
-
+            <label for="ingredienten">Ingredienten</label>
+            <input type="text" name="ingredienten"><br>
+            <label for="recept">Recept</label><br>
+            <textarea id="recept" name="recept" rows="20" cols="50"></textarea><br>
+            <input type="submit">    
         </form>
+
+
+        <?php
+
+        echo $_POST["naam_maker"] . " " . $_POST["naam_recept"] . " " . $_POST["naam_categorie"] . " " . $_POST["aantal_personen"] . " " . $_POST["ingredienten"] . "" . $_POST["recept"];
+
+        ?>
     </section>
 
 <?php
---
+++ parts/insert.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once 'db.php';
+
+if(isset($_POST["submit"])) {
+  $maker = $_POST["naam_maker"];
+  $naam_recept = $_POST["naam_recept"];
+  $categorie = $_POST["naam_categorie"];
+  $personen = $_POST["aantal_personen"];
+  $ingredienten = $_POST["ingredienten"];
+  $recept = $_POST["recept"];
+
+  $sql = "INSERT INTO `receptenboek` . `recepten` (`id`, `naam_maker`, `naam_recept`, `naam_categorie`, `aantal_personen`, `ingredienten`, `recept`) VALUES (NULL, '$maker', '$naam_recept', '$categorie', '$personen', '$ingredienten', '$recept')";
+
+  $conn->query($sql);
+
+  if (mysqli_query($conn, $sql)) {
+    echo "Recept succesvol toegevoegd";
+  } else {
+    echo "Fout: " . $sql . ":-" . mysqli_error($conn);
+  }
+
+  mysqli_close($conn);
+}
+
+?>
