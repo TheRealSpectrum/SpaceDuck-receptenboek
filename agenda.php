c42ff51481a878783150b59dcfa39c8b1a7e0df3
Patricia <poudenaarden@outlook.com>
2021-05-31 16:17:46 +0200

    Changes agenda

--- agenda.php
+++ agenda.php
@@ -1,27 +1,27 @@
 <?php
-    include("parts/header.php");
-?> 
+include("parts/header.php");
+?>
 
-  <h1 class="bg-light text-center">Team SpaceDuck Receptenboek</h1>
+<h1 class="bg-light text-center">Team SpaceDuck Receptenboek</h1>
 
-  <section class="bg-light text-center">
-    <h2>Agenda</h2>
+<section class="bg-light fs-4 text">
+  <h2 class="text-center">Agenda</h2>
 
-    <ul>
-      <li>Introductie</li>
-        <ul>
-          <li>Welkom</li>
-          <li>Voorstellen</li>
-          <li>Wat hebben we gedaan?</li>
-        </ul>
-      <li>Project</li>
-        <ul>
-          <li>Show & Tell</li>
-          <li>Technische uitleg: code</li>
-        </ul>
-      <li>Afsluiting</li>
-        <ul>
-          <li>Vragen en opmerkingen?</li>
-        </ul>
-    </ul>
-  </section>
+  <ul class="d-flex flex-column align-items-center">
+    <li>Introductie</li>
+      <ul>
+        <li>Welkom</li>
+        <li>Voorstellen</li>
+        <li>Wat hebben we gedaan?</li>
+      </ul>
+    <li>Project</li>
+      <ul>
+        <li>Show & Tell</li>
+        <li>Technische uitleg: code</li>
+      </ul>
+    <li>Afsluiting</li>
+      <ul>
+        <li>Vragen en opmerkingen?</li>
+      </ul>
+  </ul>
+</section>
